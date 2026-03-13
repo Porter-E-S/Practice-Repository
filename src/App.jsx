37ac7b22e6d0abb0db9dd1e4337fcdc8814c2ac7
Porter Sullivan <portersullivan@Porters-MacBook-Air.local>
2026-03-13 03:21:50 -0500

    anime search bar with jikan api

--- src/App.jsx
+++ src/App.jsx
@@ -1,120 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
+  //Search bar, starts empty and updates useState when neccessary.
+  const [query, setQuery] = useState('')
+  //When the search is sent, a list of animes will show up in an array.
+  const [results, setResults] = useState([])
+  //async to hold the data for a moment so the app won't run before the data is called.
+  const searchAnime = async () => {
+    const response = await fetch(`https://api.jikan.moe/v4/anime?q=${query}`)
+    const data = await response.json()
+    console.log(data.data)
+    setResults(data.data)
+  }
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    <div>
+        <h1>Anime Search</h1>
+        <input 
+          value={query}
+          //when something is searched, the event(e) will update the value for the query
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Search anime"
+          //results show up in list. map loops through every anime result in the array and returns JSX for each
+        //key keeps track of each item in the list with a unique key
+        />
+        <button onClick={searchAnime}>Search</button>
+        {results.map((anime) => (
+          <div key={anime.mal_id}>
+            <img src={anime.images.jpg.image_url} alt={anime.title} />
+            <h2>{anime.title}</h2>
+            <p>{anime.synopsis}</p>
+          </div>
+        ))}
+    </div>
   )
 }
 
